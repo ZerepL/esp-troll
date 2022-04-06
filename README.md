01429f86c27a6cad49757a807ee5c01c22cb3f99
Lucas Perez <pluqui1@gmail.com>
2022-04-06 20:15:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# esp-troll
+# Esp-troll
  Why not troll your friends?
+
+Troll your friends with a circuit that simulate a keyboard and send some pre-programmed keystrokes, like alt + tab, capslock or even a Rick roll!
+
+## Material
+ * ESP32 (Should work with esp8266 too)
+ * LCD 16x2 with I2C
+ * 3 Buttons
+ * 4 resistors (1kΩ or 220Ω, only 3 required)
+ * LED (Opcional)
+ * Breadboard
+
+## Libs Used
+ * Wire
+ * LiquidCrystal_I2C
+ * BleKeyboard
+ * ezButton
+
+## Circuit
+![Circuit](/miscs/circuit.png "Circuit")
+
+## How use
+
+Upload the esp-troll.ino code to your Esp, connect to your friend's computer and power on your Esp. You should use the buttons 34 and 39 to navegate through the panel, when you find a one that you like, simple press the action button (35).
+It works with Linux and Windows, I never tested with Mac.
+
+## Functions
+
+| Function            | Description                                                |
+|---------------------|------------------------------------------------------------|
+| Test                | Just sent a test text                                      |
+| Rick                | Send [this](https://www.youtube.com/watch?v=dQw4w9WgXcQ) and press enter                              |
+| Random              | Wait 1 or 10 minutes and send any function bellow this one |
+| Backspace           | -                                                          |
+| Home                | -                                                          |
+| alt + tab           | -                                                          |
+| Ctrl + alt + delete | -                                                          |
+| Esc                 | -                                                          |
+| Enter               | -                                                          |
+| Mute                | -                                                          |
+| Up                  | Up arrow                                                   |
+| GUI                 | Windows button                                             |
+| Caps Lock           | -                                                          |
+| Logout              | Logout the active user                                     |
+
+## Troubleshooting
+
+### My code do not upload
+
+Check if you are using the GND pin above 5V, if you are using, than change to other one, like in the Circuit pic.
+
+### I'm stuck in random mode
+
+Press "EN" to reset your Esp
+
+## TODO
+
+ * Find a way to exit from random mode.
---
+++ esp-troll.ino
@@ -0,0 +1,275 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <BleKeyboard.h>
+#include <ezButton.h>
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+BleKeyboard bleKeyboard;
+
+ezButton buttonUp(34);
+ezButton buttonDown(39);
+ezButton buttonAction(35);
+
+boolean randomMode = false;
+boolean action = false;
+
+int page = 1;
+int maxPage = 14;
+
+void setup() {  
+  lcd.init();
+  lcd.backlight();
+
+  buttonUp.setDebounceTime(50);
+  buttonDown.setDebounceTime(50);
+  buttonAction.setDebounceTime(50);
+  
+  bleKeyboard.begin();
+}
+
+void loop() {
+  buttonUp.loop();
+  buttonDown.loop();
+  buttonAction.loop();
+
+  if(buttonUp.isPressed()) {
+    if (page >= maxPage) {
+      page = maxPage;
+    } else {
+      page++;
+    }
+
+    lcd.clear();
+  }
+
+  if(buttonDown.isPressed()) {
+    if (page <= 1) {
+      page = 1;
+    } else {
+      page--;
+    }
+
+    lcd.clear();
+  }
+
+  if(buttonAction.isPressed()) {
+    action = true;
+
+    if (randomMode) {
+      randomMode = false;
+    }
+
+    lcd.clear();
+  }
+
+  lcd.setCursor(0, 0);
+
+  if (randomMode) {
+    lcd.clear();
+    page = random(4, maxPage);
+    delay(random(60000, 600000));
+  }
+  
+  switch (page) {
+    case 1:
+    
+    if (action) {
+        bleKeyboard.print("test");
+
+        action = false;
+      }
+    
+      lcd.print("test");
+      break;
+      
+    case 2:
+
+      lcd.print("rick");
+      
+      if (action) {
+        bleKeyboard.print("youtube.com/watch");
+        delay(100);
+        bleKeyboard.print("?v=dQw4w9WgXcQ");
+        delay(100);
+        bleKeyboard.press(KEY_NUM_ENTER);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+      
+    case 3:
+      lcd.print("random");
+
+      if (action) {
+        randomMode = true;
+      }
+
+      break;
+      
+    case 4:
+      lcd.print("backspace");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_BACKSPACE);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+      
+    case 5:
+      lcd.print("home");
+      
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_HOME);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+    
+      break;
+
+     case 6:
+      lcd.print("alt + tab");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_LEFT_ALT);
+        bleKeyboard.press(KEY_TAB);
+        delay(100);
+        bleKeyboard.release(KEY_TAB);
+        delay(100);
+        bleKeyboard.press(KEY_TAB);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+    
+      break;
+
+    case 7:
+      lcd.print("ctrl + alt + del");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_LEFT_CTRL);
+        bleKeyboard.press(KEY_LEFT_ALT);
+        bleKeyboard.press(KEY_DELETE);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+      
+    case 8:
+      lcd.print("esc");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_ESC);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+      
+    case 9:
+      lcd.print("enter");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_NUM_ENTER);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+
+    case 10:
+      lcd.print("mute");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_MEDIA_MUTE);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+
+    case 11:
+      lcd.print("up");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_UP_ARROW);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+
+    case 12:
+      lcd.print("GUI");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_LEFT_GUI);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+
+    case 13:
+      lcd.print("caps lock");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_CAPS_LOCK);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+
+    case 14:
+      lcd.print("logout");
+
+      if (action || randomMode) {
+        bleKeyboard.press(KEY_LEFT_CTRL);
+        bleKeyboard.press(KEY_LEFT_ALT);
+        bleKeyboard.press(KEY_DELETE);
+        delay(100);
+        bleKeyboard.releaseAll();
+        delay(100);
+        bleKeyboard.press(KEY_RIGHT_ARROW);
+        bleKeyboard.release(KEY_RIGHT_ARROW);
+        delay(100);
+        bleKeyboard.press(KEY_NUM_ENTER);
+        delay(100);
+        bleKeyboard.releaseAll();
+
+        action = false;
+      }
+      
+      break;
+  }
+  
+  lcd.setCursor(0, 1);
+  lcd.printf("Page %d", page);
+  
+}
